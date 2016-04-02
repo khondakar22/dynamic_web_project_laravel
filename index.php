ac63911a80a1e6b4559fa19f05efffa2cf677188
khondakar22 <kondakar22readul@gmail.com>
2016-04-02 18:52:39 +0200

    It is now relase to as a first version

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-echo 'first page';
-
+<html>
+    
+    <head> </head>
+    <title></title>
+    
+    <body>
+        
+        <?php echo 'the first one';?>
+        
+    </body>
+    
+    
+    
+    
+</html>
